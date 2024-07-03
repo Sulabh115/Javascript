7860cae4ff3dfcf280d1e1051247056c1b1b43bb
Sulabh115 <127303073+Sulabh115@users.noreply.github.com>
2024-07-03 10:41:31 +0000

    done

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const account=12223
+let name = "Sulabh" //use let before variables
+var email= "sulabhtharu123@gmail.com"
+accountcity = "Rajapur"
+let accountstate;
+/* prefer not to use var 
+because of issue in block space and functional scope
+*/
+
+// account=12 not allowed
+name="prabhat"
+console.log(name);
+console.table([account, name , email, accountcity, accountstate]);
